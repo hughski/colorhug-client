560903ce02dd6fef2191831e3c78ec84fb0ef53b
Richard Hughes <richard@hughsie.com>
2011-10-17 15:35:30 +0100

    Flesh out the colorhug command line tool a bit

---
+++ src/ch-main.c
@@ -0,0 +1,287 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2011 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+
+#include <glib/gi18n.h>
+#include <gio/gio.h>
+#include <locale.h>
+
+#include "ch-client.h"
+
+typedef struct {
+	ChClient		*client;
+	GOptionContext		*context;
+	GPtrArray		*cmd_array;
+} ChUtilPrivate;
+
+typedef gboolean (*ChUtilPrivateCb)	(ChUtilPrivate	*util,
+					 gchar		**values,
+					 GError		**error);
+
+typedef struct {
+	gchar		*name;
+	gchar		*description;
+	ChUtilPrivateCb	 callback;
+} ChUtilItem;
+
+/**
+ * cd_util_item_free:
+ **/
+static void
+cd_util_item_free (ChUtilItem *item)
+{
+	g_free (item->name);
+	g_free (item->description);
+	g_free (item);
+}
+
+/*
+ * cd_sort_command_name_cb:
+ */
+static gint
+cd_sort_command_name_cb (ChUtilItem **item1, ChUtilItem **item2)
+{
+	return g_strcmp0 ((*item1)->name, (*item2)->name);
+}
+
+/**
+ * cd_util_add:
+ **/
+static void
+cd_util_add (GPtrArray *array, const gchar *name, const gchar *description, ChUtilPrivateCb callback)
+{
+	gchar **names;
+	guint i;
+	ChUtilItem *item;
+
+	/* add each one */
+	names = g_strsplit (name, ",", -1);
+	for (i=0; names[i] != NULL; i++) {
+		item = g_new0 (ChUtilItem, 1);
+		item->name = g_strdup (names[i]);
+		if (i == 0) {
+			item->description = g_strdup (description);
+		} else {
+			/* TRANSLATORS: this is a command alias */
+			item->description = g_strdup_printf (_("Alias to %s"),
+							     names[0]);
+		}
+		item->callback = callback;
+		g_ptr_array_add (array, item);
+	}
+	g_strfreev (names);
+}
+
+/**
+ * cd_util_get_descriptions:
+ **/
+static gchar *
+cd_util_get_descriptions (GPtrArray *array)
+{
+	guint i;
+	guint j;
+	guint len;
+	guint max_len = 0;
+	ChUtilItem *item;
+	GString *string;
+
+	/* get maximum command length */
+	for (i=0; i<array->len; i++) {
+		item = g_ptr_array_index (array, i);
+		len = strlen (item->name);
+		if (len > max_len)
+			max_len = len;
+	}
+
+	/* ensure we're spaced by at least this */
+	if (max_len < 19)
+		max_len = 19;
+
+	/* print each command */
+	string = g_string_new ("");
+	for (i=0; i<array->len; i++) {
+		item = g_ptr_array_index (array, i);
+		g_string_append (string, "  ");
+		g_string_append (string, item->name);
+		len = strlen (item->name);
+		for (j=len; j<max_len+3; j++)
+			g_string_append_c (string, ' ');
+		g_string_append (string, item->description);
+		g_string_append_c (string, '\n');
+	}
+
+	/* remove trailing newline */
+	if (string->len > 0)
+		g_string_set_size (string, string->len - 1);
+
+	return g_string_free (string, FALSE);
+}
+
+/**
+ * cd_util_run:
+ **/
+static gboolean
+cd_util_run (ChUtilPrivate *priv, const gchar *command, gchar **values, GError **error)
+{
+	gboolean ret = FALSE;
+	guint i;
+	ChUtilItem *item;
+	GString *string;
+
+	/* find command */
+	for (i=0; i<priv->cmd_array->len; i++) {
+		item = g_ptr_array_index (priv->cmd_array, i);
+		if (g_strcmp0 (item->name, command) == 0) {
+			ret = item->callback (priv, values, error);
+			goto out;
+		}
+	}
+
+	/* not found */
+	string = g_string_new ("");
+	/* TRANSLATORS: error message */
+	g_string_append_printf (string, "%s\n",
+				_("Command not found, valid commands are:"));
+	for (i=0; i<priv->cmd_array->len; i++) {
+		item = g_ptr_array_index (priv->cmd_array, i);
+		g_string_append_printf (string, " * %s\n", item->name);
+	}
+	g_set_error_literal (error, 1, 0, string->str);
+	g_string_free (string, TRUE);
+out:
+	return ret;
+}
+
+/**
+ * cd_util_get_multiplier:
+ **/
+static gboolean
+cd_util_get_multiplier (ChUtilPrivate *priv, gchar **values, GError **error)
+{
+	gboolean ret;
+	ChFreqScale multiplier;
+
+	/* get from HW */
+	ret = ch_client_get_multiplier (priv->client, &multiplier, error);
+	if (!ret)
+		goto out;
+
+	switch (multiplier) {
+	case CH_FREQ_SCALE_0:
+		g_print ("0%% (disabled)");
+		break;
+	case CH_FREQ_SCALE_2:
+		g_print ("2%%");
+		break;
+	case CH_FREQ_SCALE_20:
+		g_print ("20%%");
+		break;
+	case CH_FREQ_SCALE_100:
+		g_print ("100%%");
+		break;
+	default:
+		ret = FALSE;
+		g_set_error (error, 1, 0,
+			     "invalid multiplier value %i",
+			     multiplier);
+	}
+out:
+	return ret;
+}
+
+/**
+ * main:
+ **/
+int
+main (int argc, char *argv[])
+{
+	gboolean ret;
+	GError *error = NULL;
+	guint retval = 1;
+	ChUtilPrivate *priv;
+	gchar *cmd_descriptions = NULL;
+
+	setlocale (LC_ALL, "");
+
+	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
+
+	g_type_init ();
+
+	/* create helper object */
+	priv = g_new0 (ChUtilPrivate, 1);
+
+	/* add commands */
+	priv->cmd_array = g_ptr_array_new_with_free_func ((GDestroyNotify) cd_util_item_free);
+	cd_util_add (priv->cmd_array,
+		     "get-multiplier",
+		     /* TRANSLATORS: command description */
+		     _("Gets the sensor multiplier"),
+		     cd_util_get_multiplier);
+
+	/* sort by command name */
+	g_ptr_array_sort (priv->cmd_array,
+			  (GCompareFunc) cd_sort_command_name_cb);
+
+	/* get a list of the commands */
+	priv->context = g_option_context_new (NULL);
+	cmd_descriptions = cd_util_get_descriptions (priv->cmd_array);
+	g_option_context_set_summary (priv->context, cmd_descriptions);
+
+	/* TRANSLATORS: program name */
+	g_set_application_name (_("Color Management"));
+	g_option_context_parse (priv->context, &argc, &argv, NULL);
+
+	/* get connection to colord */
+	priv->client = ch_client_new ();
+	ret = ch_client_load (priv->client, &error);
+	if (!ret) {
+		/* TRANSLATORS: no colord available */
+		g_print ("%s %s\n", _("No connection to device:"),
+			 error->message);
+		g_error_free (error);
+		goto out;
+	}
+
+	/* run the specified command */
+	ret = cd_util_run (priv, argv[1], (gchar**) &argv[2], &error);
+	if (!ret) {
+		g_print ("%s\n", error->message);
+		g_error_free (error);
+		goto out;
+	}
+
+	/* success */
+	retval = 0;
+out:
+	if (priv != NULL) {
+		g_object_unref (priv->client);
+		if (priv->cmd_array != NULL)
+			g_ptr_array_unref (priv->cmd_array);
+		g_option_context_free (priv->context);
+		g_free (priv);
+	}
+	g_free (cmd_descriptions);
+	return retval;
+}
+
